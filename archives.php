8ea5027c7426cae46d1a677470d230158e7b4763
Robert Deniszczyc <robdeniszczyc@gmail.com>
2015-08-14 15:30:42 +0100

    content margins

--- archives.php
+++ archives.php
@@ -6,14 +6,16 @@
 		<?php the_post(); ?>
 		<h1 class="entry-title"><?php the_title(); ?></h1>
 		
-		<ul class="list-square">
-		<?php
-                    $recent_posts = wp_get_recent_posts();
-                    foreach( $recent_posts as $recent ){
-                            echo '<li><a href="' . get_permalink($recent["ID"]) . '" title="Look '.esc_attr($recent["post_title"]).'" >' .   $recent["post_title"].'</a></li>';
-                    }
-		?>
-                </ul>
+                <section class="primary-content_the-content">
+                    <ul class="list-square">
+                    <?php
+                        $recent_posts = wp_get_recent_posts();
+                        foreach( $recent_posts as $recent ){
+                                echo '<li><a href="' . get_permalink($recent["ID"]) . '" title="Look '.esc_attr($recent["post_title"]).'" >' .   $recent["post_title"].'</a></li>';
+                        }
+                    ?>
+                    </ul>
+                </section>
 	</div>
 </article>
 
--- home.php
+++ home.php
@@ -3,7 +3,7 @@
     <div class="banner_home">
         <div class="banner-heading">  
             <div class="banner-text-container">
-                <h1 class="huge">Robert<br />Deniszczyc</h1>
+                <h1 class="huge banner-name">Robert<br />Deniszczyc</h1>
                 <h2 class="echo"><a href="/cv">Web developer</a>, <a href="http://instagram.com/masterrobert">Instagrammer</a>, <a href="http://www.airlinereporter.com/2013/07/what-is-an-avgeek-i-am-an-avgeek-hear-me-roar">#avgeek</a></h2>
                 <p>
                     Web developer from Sheffield, South Yorkshire. Graduate of Computer Information Systems BSc (Hons) from the <br /><a href="http://lincoln.ac.uk">University of Lincoln.</a>
